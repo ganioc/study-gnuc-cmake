971680f591834b936569f895d47933d97f994dae
yango <o3.yang@hotmail.com>
2025-01-22 09:53:54 +0800

    Copilot is working in my IDE

--- helloc/hello.c
+++ helloc/hello.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 
+#define SIZE 5
+
+// local function to print out elements of an array
+void print_array(int arr[], int size){
+    for(int i = 0; i < size; i++){
+        printf("%d\n", arr[i]);
+    }
+}
+
+
 int main(void){
     // local int array
+    int arr[SIZE] = {1, 2, 3, 4, 5};
+
+    // print array elements one by one
+    print_array(arr, SIZE);
+
+    // change element of arr[], arr[i]*2
+    for(int i = 0; i < SIZE; i++){
+        arr[i] *= 2;
+    }
+
+    print_array(arr, SIZE);
+
     printf("Hello C\n");
 
     return 0;
